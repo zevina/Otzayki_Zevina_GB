35bf8944617393d69e252548b27319e91d9d1a2a
Zevina Lena <zevina.l@yandex.ru>
2024-03-20 17:15:51 +0700

    Cafe page added

--- src/pages/CafePage.vue
+++ src/pages/CafePage.vue
@@ -1,15 +1,124 @@
 <template>
-    <div class="center">
-        <section class="top center">
-            <div class="top__img">
-                <picture>
-                    <source media="(min-width: 1200px)" srcset='@/assets/img/cafe/cafe_bg_big.png'>
-                    <source media="(min-width: 960px)" srcset='@/assets/img/cafe/cafe_bg_middle.png'>
-                    <img class="top__bg" alt="photo of desserts" src='@/assets/img/cafe/cafe_bg_small.png'>
-                </picture>
-            </div>
-        </section>
+    <div class="top__img">
     </div>
+    <div class="wrapper-drinks center">
+        <div class="drinks">
+            <div class="drinks__title">Напитки</div>
+            <div class="drinks__subtitle">каждый найдет что-то по душе</div>
+            <div class="drinks__content">
+                <div class="drinks__content_left">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset='@/assets/img/cafe/cafe_drinks_big.png'>
+                        <source media="(min-width: 960px)" srcset='@/assets/img/cafe/cafe_drinks_middle.png'>
+                        <img class="drinks__content__img" alt="photo of desserts"
+                            src='@/assets/img/cafe/cafe_drinks_small.png'>
+                    </picture>
+                </div>
+                <div class="drinks__content_right">
+                    <div class="drinks__content_section">
+                        <div class="drinks__content_item">
+                            <span class='label'>Эспрессо</span> <span class='price'>100 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Капучино</span> <span class='price'>160\200 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Латте</span> <span class='price'>180 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Флэт Уайт</span> <span class='price'>190 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Раф цитрусовый</span> <span class='price'>230 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Американо</span> <span class='price'>150 ₽</span>
+                        </div>
+                    </div>
+                    <div class="drinks__content_section">
+                        <div class="drinks__content_item">
+                            <span class='label'>Чай классический</span> <span class='price'>200 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Чай авторский</span> <span class='price'>250 ₽</span>
+                        </div>
+                    </div>
+                    <div class="drinks__content_section">
+                        <div class="drinks__content_item">
+                            <span class='label'>Лимонад (Австралия)</span> <span class='price'>350 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Лимонад Лапочка</span> <span class='price'>150 ₽</span>
+                        </div>
+                    </div>
+                    <div class="drinks__content_section">
+                        <div class="drinks__content_item">
+                            <span class='label'>Раф цикорий</span> <span class='price'>100 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Капучино цикорий</span> <span class='price'>100 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Чай классический</span> <span class='price'>100 ₽</span>
+                        </div>
+                        <div class="drinks__content_item">
+                            <span class='label'>Чай авторский</span> <span class='price'>100 ₽</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="drinks__decor">
+            <picture>
+                <source media="(min-width: 1200px)" srcset='@/assets/img/cafe/cafe_rosemary_big.svg'>
+                <source media="(min-width: 960px)" srcset='@/assets/img/cafe/cafe_rosemary_middle.svg'>
+                <img class="drinks__decor__img" alt="photo of desserts" src='@/assets/img/cafe/cafe_rosemary_small.svg'>
+            </picture>
+        </div>
+    </div>
+    <div class="wrapper-desserts">
+        <div class="desserts center">
+            <div class="desserts__title">Десерты</div>
+            <div class="desserts__subtitle">для наших любимых сладкоежек</div>
+            <div class="desserts__content">
+                <div class="desserts__content_left">
+                    <div class="desserts__content_item">
+                        <span class='label'>Пирожное Прага</span> <span class='price'>290 ₽</span>
+                    </div>
+                    <div class="desserts__content_item">
+                        <span class='label'>Пирожное Медовик</span> <span class='price'>260 ₽</span>
+                    </div>
+                    <div class="desserts__content_item">
+                        <span class='label'>Конфета Трюфель</span> <span class='price'>90 ₽</span>
+                    </div>
+                    <div class="desserts__content_item">
+                        <span class='label'>Меренговый рулет с клубникой</span> <span class='price'>350 ₽</span>
+                    </div>
+                    <div class="desserts__content_item">
+                        <span class='label'>Торт морковный</span> <span class='price'>260 ₽</span>
+                    </div>
+                </div>
+                <div class="desserts__content_right">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset='@/assets/img/cafe/cafe_desserts_big.png'>
+                        <source media="(min-width: 960px)" srcset='@/assets/img/cafe/cafe_desserts_middle.png'>
+                        <img class="desserts__content__img" alt="photo of desserts"
+                            src='@/assets/img/cafe/cafe_desserts_small.png'>
+                    </picture>
+                </div>
+            </div>
+        </div>
+        <div class="desserts__decor">
+            <picture>
+                <source media="(min-width: 1200px)" srcset='@/assets/img/cafe/cafe_mint_big.svg'>
+                <source media="(min-width: 960px)" srcset='@/assets/img/cafe/cafe_mint_middle.svg'>
+                <img class="desserts__decor__img" alt="photo of desserts" src='@/assets/img/cafe/cafe_mint_small.svg'>
+            </picture>
+        </div>
+    </div>
+
+
+
 </template>
 
 <script>
